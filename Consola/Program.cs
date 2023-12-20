b846a3aa85416a7c5bb248486459d05adf27f889
abrilchauq <abril.chauqueet12d1@gmail.com>
2023-12-20 16:41:25 -0300

    cambios

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -3,11 +3,18 @@ using Dominio.Interfaz;
 
 List<Libro> ListaLibros = new List<Libro>();
 
-Estante estante = new Estante();
+Estante estante1 = new Estante();
+Estante estante2 = new Estante();
+Estante estante3 = new Estante();
+Estante estante4 = new Estante();
+Estante estante5 = new Estante();
 
 Libro libroC = new Libro("Introduccion a c#", "John Doe");
+Libro libroC2 = new Libro("Python", "Desconocido");
+ListaLibros.Add(libroC2);
 ListaLibros.Add(libroC);
 ITematica computacion = new Computacion(libroC);
+ITematica computacion2 = new Computacion(libroC2);
 
 Libro libroN = new Libro("Harry Potter: El caliz de fuego", "J.K Rowling");
 ListaLibros.Add(libroN);
@@ -25,16 +32,25 @@ Libro libroT = new Libro("El visitante", "Stephen King");
 ListaLibros.Add(libroT);
 ITematica terror = new Deportes(libroT);
 
-estante.AgregarLibro(computacion);
-estante.AgregarLibro(novela);
-estante.AgregarLibro(deportes);
-estante.AgregarLibro(historia);
-estante.AgregarLibro(terror);
+estante1.AgregarLibro(computacion);
+estante1.AgregarLibro(computacion2);
+estante2.AgregarLibro(novela);
+estante3.AgregarLibro(deportes);
+estante4.AgregarLibro(historia);
+estante5.AgregarLibro(terror);
 
-Console.WriteLine("------------------");
-Console.WriteLine("LIBROS EN ESTANTES: ");
-Console.WriteLine("------------------");
-foreach (var libro in estante.ObtenerLibros())
-{
-    libro.ObtenerInformacion();
-}
+Console.WriteLine($"Estante computación: ");
+estante1.MostrarLibros();
+
+Console.WriteLine($"Estante novelas: ");
+estante2.MostrarLibros();
+
+Console.WriteLine($"Estante deportes: ");
+estante3.MostrarLibros();
+
+Console.WriteLine($"Estante historia: ");
+estante4.MostrarLibros();
+
+Console.WriteLine($"Estante terror: ");
+estante5.MostrarLibros();
+
